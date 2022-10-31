18feb0223e2561d8a4e24a7a19839f7348fbcee5
kyli <ldh71777@gmail.com>
2022-10-31 15:21:23 +0800

    feat: test case

---
+++ tests/integration/runtime/fixtures/config-routes/src/routes.ts
@@ -0,0 +1,32 @@
+// import { DefinedRoutes } from '@modern-js/runtime/router';
+
+const routes = [
+  {
+    path: '/',
+    component: '@/Layout',
+    title: 'home',
+    children: [
+      {
+        path: 'apple',
+        component: '@/pages/Apple',
+        title: 'apple',
+      },
+      {
+        path: 'banana/:id',
+        component: '@/pages/Banana',
+        title: 'banana',
+      },
+    ],
+  },
+  {
+    path: 'toapple',
+    redirect: '/apple',
+    component: '@/pages/Cat',
+  },
+  {
+    path: '*',
+    component: '@/404',
+    title: '404',
+  },
+];
+module.exports = routes;
